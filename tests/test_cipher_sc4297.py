f95c3f89c05156b21427856a5ec592e3d82bf585
Beth Choi <sc4297@columbia.edu>
2022-11-01 23:55:32 -0700

    3

--- tests/test_cipher_sc4297.py
+++ tests/test_cipher_sc4297.py
@@ -1,1 +1,9 @@
 from cipher_sc4297 import cipher_sc4297
+
+def test_cipher_word():
+    text = 'Zebra'
+    shift = 2
+    encrypt = True
+    expected = 'bgdtc'
+    actual = cipher_sc4297.cipher(text, shift, encrypt)
+    assert actual == expected
